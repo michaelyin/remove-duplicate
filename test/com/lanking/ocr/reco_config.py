82706823c474dfd0a309f7ab1749de076a620f3f
michaelyin <gt8442b@yahoo.com>
2019-04-14 16:49:47 -0400

    add comment.

--- test/com/lanking/ocr/reco_config.py
+++ test/com/lanking/ocr/reco_config.py
@@ -1,3 +1,4 @@
+## DONT checkin this file; the _key is not supposed to be checked in repo
 
 _url = 'https://westus.api.cognitive.microsoft.com/vision/v1.0/RecognizeText'
-_key = 'your key here'
+_key = 'your key'
